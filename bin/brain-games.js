76ec73ea919e28cfc44a7f3bca0582ebf69aeac9
igor Nikitin <ingvyn@yandex.ru>
2021-03-23 13:29:37 +0000

    добавлен функционал, считывающий ввод имени пользователя, как отдельный модуль

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+import setUser from '../src/cli.js';
 console.log('Welcome to the Brain Games!')
+setUser();
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const setUser = ( nameRequestMessage = 'May I have your name? ' ) => {
+    const name = readlineSync.question( nameRequestMessage );
+    console.log(`Hello, ${name}!`);
+};
+export default setUser;
