dde71b1c1f47aa4b25a225fdfb596366d3a11a39
igor Nikitin <ingvyn@yandex.ru>
2021-03-24 08:55:31 +0000

    Добавил eslint и правила - внес правки по линтеру

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import setUser from '../src/cli.js';
-console.log('Welcome to the Brain Games!')
+
+console.log('Welcome to the Brain Games!');
 setUser();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
-const setUser = ( nameRequestMessage = 'May I have your name? ' ) => {
-    const name = readlineSync.question( nameRequestMessage );
-    console.log(`Hello, ${name}!`);
+
+const setUser = (nameRequestMessage = 'May I have your name? ') => {
+  const name = readlineSync.question(nameRequestMessage);
+  console.log(`Hello, ${name}!`);
 };
 export default setUser;
