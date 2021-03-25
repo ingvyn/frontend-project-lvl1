774811416908407f249078771281898726be41d5
igor Nikitin <ingvyn@yandex.ru>
2021-03-25 18:52:35 +0000

    исправил случайный ввод в brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,8 +4,3 @@ import getUserResponse from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
 const userName = getUserResponse('May I have your name? ');
 console.log(`Hello, ${userName}!`);
-let countQuestions = 0;
-let userAnswer;
-do {
-  countQuestions += 1;
-}
