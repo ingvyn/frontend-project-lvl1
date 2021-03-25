9cddd1e748a33630252369657b271e4220a84450
igor Nikitin <ingvyn@yandex.ru>
2021-03-25 18:42:24 +0000

    написал логику консольного ввода-вывода для brain-even модуля

---
+++ bin/brain-even.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import getUserResponse from '../src/cli.js';
+import getRandomArbitrary from '../src/randomNumber.js';
+import isEven from '../src/evenNumber.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = getUserResponse('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let countQuestions = 0;
+let userAnswer;
+let nextNumber;
+let correctAnswer;
+let isAnswerCorrect = false;
+do {
+  countQuestions += 1;
+  nextNumber = getRandomArbitrary(1, 29);
+  correctAnswer = isEven(nextNumber) ? 'yes' : 'no';
+  userAnswer = getUserResponse(`Question: ${nextNumber}`);
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    isAnswerCorrect = true;
+  } else {
+    console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+    console.log(`Let's try again, ${userName}!`);
+    isAnswerCorrect = false;
+    break;
+  }
+} while (countQuestions < 3);
+if (isAnswerCorrect) {
+  console.log(`Congratulations, ${userName}!`);
+}
---
+++ src/randomNumber.js
@@ -0,0 +1,2 @@
+const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
+export default getRandomArbitrary;
