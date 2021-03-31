b16aa4138218edbd8846c296637b6a4ae9415149
IgorNick <ingvyn@yandex.ru>
2021-03-31 15:45:59 +0300

    оптимизировал раунд brain-gcd для обращения к функции gcd с убывающими аргументами

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,12 +1,12 @@
 import { getRandomNumber, gcd } from '../math.js';
 
 const setGcdRound = () => {
-  const operandOne = getRandomNumber();
-  const operandTwo = getRandomNumber();
-  const question = `${operandOne} ${operandTwo}`;
-  let correctAnswer = (operandOne > operandTwo) ? gcd(operandOne, operandTwo)
-    : gcd(operandTwo, operandOne);
-  correctAnswer = String(correctAnswer);
+  const operands = [];
+  operands.push(getRandomNumber());
+  operands.push(getRandomNumber());
+  const question = `${operands[0]} ${operands[1]}`;
+  operands.sort((a, b) => b - a);
+  const correctAnswer = String(gcd(...operands));
   return { question, correctAnswer };
 };
 const description = 'Find the greatest common divisor of given numbers.';
