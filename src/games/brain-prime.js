ad613ab473b0cc9fac40f6fa21e396dc174eeb6f
IgorNick <ingvyn@yandex.ru>
2021-04-02 15:00:40 +0300

    fix function isPrime in game module brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,7 +2,7 @@ import { getRandomNumber } from '../math.js';
 
 const isPrime = (number) => {
   if (number === 1) {
-    return false;
+    return 'no';
   }
   for (let part = 2; part <= number / 2; part += 1) {
     if (number % part === 0) {
